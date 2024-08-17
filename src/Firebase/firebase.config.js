834a3b0746cf9b2033f0d96ec056d41305b63379
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-08-17 17:44:46 +0600

    hiding key

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHjyUMyykfbJ6FDy6FGkklwkK5nhvD4Hs",
-  authDomain: "dolt-task.firebaseapp.com",
-  projectId: "dolt-task",
-  storageBucket: "dolt-task.appspot.com",
-  messagingSenderId: "464694248503",
-  appId: "1:464694248503:web:734315002455efdf6a2bb2"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
